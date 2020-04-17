8d58f7dadf799c95b5b150af24aadbc9f0ac6ea4
Branimir Ričko <branimir.ricko@fer.hr>
2020-04-18 01:08:41 +0200

    good

--- custom/src/styles/main.js
+++ custom/src/styles/main.js
@@ -4,4 +4,8 @@
 if (!document.querySelectorAll(".inheritedMembers div").length) {
     document.querySelector(".inheritedMembers").remove();
 }
-document.querySelectorAll(".")
+document.querySelectorAll(".contribution-panel .fab.btn-warning.pull-right").forEach(i => {
+    const old = i.getAttribute("href");
+    const newHrf = old.replace("/units-core/", "/units-core-docfx/").replace("/apiSpec/", "/apidoc/");
+    i.setAttribute("href", newHrf);
+});
