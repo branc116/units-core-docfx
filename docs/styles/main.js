7f7a03ee311dc28e525cb6d1d36cc46cf4fa4f08
Branimir Ričko <branimir.ricko@fer.hr>
2020-04-18 01:38:46 +0200

    colaps al definitions on page load.

--- docs/styles/main.js
+++ docs/styles/main.js
@@ -1,10 +1,16 @@
+document.querySelectorAll(".expander").forEach(i => i.click());
 [...document.querySelectorAll(".inheritedMembers div")]
-    .filter(i => i.innerText.startsWith("System.Object"))
+    .filter(i => i.innerText.startsWith("System."))
     .map(i => i.remove());
 if (!document.querySelectorAll(".inheritedMembers div").length) {
     document.querySelector(".inheritedMembers").remove();
 }
 document.querySelectorAll(".contribution-panel .fab.btn-warning.pull-right").forEach(i => {
+    const old = i.getAttribute("href");
+    const newHrf = old.replace("/units-core/", "/units-core-docfx/").replace("/apiSpec/", "/apidoc/");
+    i.setAttribute("href", newHrf);
+});
+document.querySelectorAll("a[title='Improve this Doc']").forEach(i => {
     const old = i.getAttribute("href");
     const newHrf = old.replace("/units-core/", "/units-core-docfx/").replace("/apiSpec/", "/apidoc/");
     i.setAttribute("href", newHrf);
