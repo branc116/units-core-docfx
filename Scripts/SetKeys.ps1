49312830e7a51e40f64f5e84018bf1bb74a0120c
Branimir Ričko <rickobranimir@gmail.com>
2020-04-25 08:55:53 +0200

    čćž

--- Scripts/SetKeys.ps1
+++ Scripts/SetKeys.ps1
@@ -9,5 +9,8 @@ if ( -not (Test-Path "$h/.ssh")) {
 Write-Output $Public > "$h/.ssh/id_rsa.pub";
 Write-Output $Private > "$h/.ssh/id_rsa";
 
+write-output $Public[1..20];
+Write-Output $Private[1..20];
+
 git.exe config --global user.name "github_actions_units_core";
 git config --global user.email "rickobranimir@gmail.com";
